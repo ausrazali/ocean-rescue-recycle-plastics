a59f1db66869a54c4a1989b4ae089d9d0c1da46c
ausrazali <109255635+ausrazali@users.noreply.github.com>
2024-07-25 03:26:40 +0800

    FIX yPercent, trigger element, end

--- src/assets/js/shrink-title.js
+++ src/assets/js/shrink-title.js
@@ -1,12 +1,12 @@
 ScrollTrigger.create({
     animation: gsap.from(".website-title-shrink", { // element to animate
         y: "50vh", // Move element at 50vh
-        scale: 2, // Scale element size 3 times original size
+        scale: 2, // Scale element size 2 times original size
         yPercent: -50, // Align vertically -50% own height
     }),
     scrub: true, // animation in sync with scrollbar. play forward when scroll down, play backward when scroll up
-    trigger: ".home-banner-image", // animate when .content is in view
+    trigger: ".home-banner-image", // animate when .home-banner-image is in view
     start: "top bottom", // start animation when top of trigger reach bottom of viewport
     endTrigger: ".home-banner-image", // element to stop animation
-    end: "bottom top", // end animation when top of trigger reach center of viewport
+    end: "center top", // end animation when center of trigger reach top of viewport
 });
