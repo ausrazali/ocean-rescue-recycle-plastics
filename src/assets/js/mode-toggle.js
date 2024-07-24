4f094e218959534ed4ad17174595edd31e7cb4a9
ausrazali <109255635+ausrazali@users.noreply.github.com>
2024-07-24 18:33:24 +0800

    ADD toggle color for toggleBtnSlider

--- src/assets/js/mode-toggle.js
+++ src/assets/js/mode-toggle.js
@@ -1,10 +1,15 @@
 const toggleBtn = document.querySelector("#toggle-btn");
+const toggleBtnSlider = document.querySelector(".slider")
 const toggleFont = document.querySelectorAll(".toggle-ft");
 const toggleBackground = document.querySelectorAll(".toggle-bg");
 const toggleBorder = document.querySelectorAll(".toggle-border");
 
 toggleBtn.addEventListener('change', () => {
     if (toggleBtn.checked) {
+        toggleBtnSlider.classList.toggle("dark-bg");
+        toggleBtnSlider.classList.toggle("light-bg");
+        toggleBtnSlider.classList.toggle("dark-ft");
+        toggleBtnSlider.classList.toggle("light-ft");
         for (let i = 0; i < toggleFont.length; i++) {
             toggleFont[i].classList.toggle('dark-ft');
             toggleFont[i].classList.toggle('light-ft');
@@ -18,6 +23,10 @@ toggleBtn.addEventListener('change', () => {
             toggleBorder[i].classList.toggle('light-border');
         }
     } else {
+        toggleBtnSlider.classList.toggle("dark-bg");
+        toggleBtnSlider.classList.toggle("light-bg");
+        toggleBtnSlider.classList.toggle("dark-ft");
+        toggleBtnSlider.classList.toggle("light-ft");
         for (let i = 0; i < toggleFont.length; i++) {
             toggleFont[i].classList.toggle('dark-ft');
             toggleFont[i].classList.toggle('light-ft');
